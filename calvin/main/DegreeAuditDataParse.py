7a9bf33916e3183e2108b59cac3fc8d002bce2bf
Calvin Schmeichel <calvinschmeichel@yahoo.com.com>
2023-11-11 16:45:14 -0600

    added items to gitignore

--- calvin/main/DegreeAuditDataParse.py
+++ calvin/main/DegreeAuditDataParse.py
@@ -3,10 +3,10 @@ def GetDegreeAudit(DegreeAutiFile):
     with open(DegreeAutiFile, 'r') as file:
         file_contents = file.read()
         
-    
     CoursesTaken = file_contents.split()
 
     return(CoursesTaken)
+
 
 def GetTakenCourses(CoursesTaken):
     courses = [["ACCT"],["ANTH"],["ABA"],["ACR"],["ART"],["ASTR"],["AHS"],["BMB"],["BIOL"],["BRIT"],["BLAW"],["CHEM"],["CFS"],["CHIN"],["CCSD"],["COLL"],["CSD"],["CMST"],["CPSY"],["CMTY"],["CSCI"],["CM"],["COUN"],["CJS"],["CYB"],["DLA"],["EAST"],["ECON"],["ED"],["EDAD"],["ECE"],["EM"],["ENGL"],["EAP"],["ENTR"],["ENVE"],["ETS"],["ETHS"],["EXSC"],["FS"],["FIRE"],["FREN"],["GWS"],["GENG"],["GEOG"],["GER"],["GERO"],["GLST"],["HLTH"],["HPE"],["HBS"],["HIED"],["HIST"],["HONS"],["HTSM"],["HURL"],["IA"],["IM"],["IS"],["INTL"],["JPN"],["JWST"],["LC"],["LIB"],["MBA"],["MGMT"],["MFET"],["MKTG"],["MFT"],["MCOM"],["MPA"],["MATH"],["MME"],["MLS"],["MTQ"],["MILS"],["MUSE"],["MUSM"],["MUSP"],["NMDT"],["NURS"],["PHIL"],["PESS"],["PHYS"],["POL"],["PCOM"],["PSY"],["RADT"],["REC"],["RAS"],["RHAB"],["REL"],["SCHL"],["SOTA"],["SCI"],["STEM"],["SSE"],["SW"],["SOC"],["SE"],["SPAN"],["SPED"],["STAT"], ["TH"],["TSE"]]
@@ -23,26 +23,22 @@ def GetTakenCourses(CoursesTaken):
             filtered_words_with_next.append(CoursesTaken[i + 1])  # Add the next item
     
     return(filtered_words_with_next)
-
-
     
 
 def PrintToScreen(filtered_words_with_next):
-   
-    #print(filtered_words_with_next)
-    #print(len(filtered_words_with_next))
+    FinalList = []
 
     for n in range (0, len(filtered_words_with_next)-1, 2):
-        print(filtered_words_with_next[n], filtered_words_with_next[n+1])
+        FinalList.append(str(filtered_words_with_next[n])+str(filtered_words_with_next[n+1]))
 
-    print(len(filtered_words_with_next))
-    print(len(set(filtered_words_with_next)))
+    return(FinalList)
 
+def GetListToTransfer(DegreeAutiFile):
+    return(PrintToScreen(GetTakenCourses(GetDegreeAudit(DegreeAutiFile))))
 
 
 def main():
-    PrintToScreen(GetTakenCourses(GetDegreeAudit("calvintrancopy.txt")))
-
+    print(GetListToTransfer("calvintrancopy.txt"))
 
 
 #This is so we can import the functions in this program in other code without having to run the main function

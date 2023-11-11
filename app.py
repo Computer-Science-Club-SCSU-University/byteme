640c3a8a4fdcc8a081fc73c584ef4d5ae59042b0
williammunnich <williammunnich@pm.me>
2023-11-10 21:16:10 -0600

    loggin into eservices

--- app.py
+++ app.py
@@ -12,19 +12,77 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
+#For opening THE .env file
+import os
+from dotenv import load_dotenv
+# Load the .env file
+load_dotenv()
+#set variables from .env file
+password = os.getenv("PASSWORD")
+username = os.getenv("USERNAME")
+
 #undetected_chromedriver as uc application, starts blank browser controlled from this python file
 driver = uc.Chrome()
 
+# Create a WebDriverWait instance with a timeout (e.g., 30 seconds)
+wait = WebDriverWait(driver, 30)
+
 #from the already started blank Chrome browser navigates to chat GPT website
-button_on_page = driver.get("https://en.wikipedia.org/wiki/Linus_Torvalds")
+button_on_page = driver.get("https://eservices.minnstate.edu/esession/authentication.do?campusId=073&postAuthUrl=http%3A%2F%2Feservices.minnstate.edu%2Fstudent-portal%2Fsecure%2Fdashboard.do%3Fcampusid%3D073")
 
 time.sleep(4)
 
-button_on_page = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div[1]/p[2]/a[6]')
+"""button_on_page = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[3]/td/select')
 button_on_page.click()
 time.sleep(4)
-# Find all paragraph elements on the page
-paragraphs = driver.find_element(By.XPATH,'/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[3]')
-print("test: ",paragraphs.text)
+button_on_page = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[3]/td/select/option[33]')
+button_on_page.click()
+time.sleep(4)"""
+input_field_username = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[1]/td/input')
+input_field_password = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[2]/td/input')
+input_field_username.send_keys(username)
+input_field_password.send_keys(password)
+button_on_page = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[5]/td[2]/input')
+button_on_page.click()
+move_on = input("Move on 1 -->")
+button_on_page = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/ul/li[5]/a')
+button_on_page.click()
+move_on = input("Move on 2 -->")
+button_on_page = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[2]/div/ul/li[1]/a')
+button_on_page.click()
+move_on = input("Move on 3 -->")
+button_on_page = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div/p[3]/strong[1]/a')
+button_on_page.click()
+move_on = input("Move on 4 -->")
+#breaking here
+input_field_username2 = driver.find_element(By.XPATH, '/html/body/main/div/div/div/form/div[1]/input')
+move_on = input("Move on 5 -->")
+input_field_password2 = driver.find_element(By.XPATH, '//*[@id="password"]')
+move_on = input("Move on 6 -->")
+input_field_username2.send_keys(username)
+move_on = input("Move on 7 -->")
+input_field_password2.send_keys(password)
+move_on = input("Move on 8 -->")
+button_on_page = driver.find_element(By.XPATH, '/html/body/main/div/div/div/form/div[3]/button')
+button_on_page.click()
+move_on = input("Move on 9 -->")
+button_on_page = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/select')
+button_on_page.click()
+move_on = input("Move on 10 -->")
+button_on_page = driver.find_element(By.XPATH, '//*[@value="73:Instidq#002362  :Instid#   :Instcd"]')
+button_on_page.click()
+move_on = input("Move on 11 -->")
+button_on_page = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/button')
+button_on_page.click()
+move_on = input("Move on 13 -->")
+button_on_page = driver.find_element(By.XPATH, '/html/body/div[2]/div/form/div[4]/div/p/button')
+button_on_page.click()
+move_on = input("Move on 14 -->")
+button_on_page = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/form[2]/div[2]/table/tbody/tr[3]/td[11]/a')
+button_on_page.click()
+move_on = input("Move on 15 -->")
+button_on_page = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[4]/div[1]/p[2]/a')
+button_on_page.click()
+move_on = input("Move on 16 -->")
 
 time.sleep(20)

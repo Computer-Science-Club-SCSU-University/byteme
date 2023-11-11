918b46430eb2383bfa5fcf6d38b06f17f93999ea
unknown <chermackbrendan@gmail.com>
2023-11-11 12:02:00 -0600

    ajax stuff

--- src/script.js
+++ src/script.js
@@ -1,16 +1,39 @@
+// JavaScript function to make an AJAX call
+const loadData = () => {
+	$.ajax({
+		url: '[place holder]', // URL of the server-side script
+        type: 'GET', // The HTTP method
+        dataType: 'json',
+		success: function (response) {
+			// This function is called if the request was successful.
+			$('#result').html(response); // Update the content of a div with id 'result'
+		},
+		error: function (xhr, status, error) {
+			// This function is called if the request failed.
+			console.error('Error: ' + error);
+		},
+	});
+}
+// Call the function, for example, on a button click
+$('#goals-needed').click(loadData);
+
+
+
+
+
+
+
 const displayData = (data) => {
-    // Use the jQuery $ function to select the element by ID and set its content
-    $('#goals-needed').text(data); // If it's just text
-    // OR
-    //$('#data-container').html(data); // If you need to insert HTML
+	// Use the jQuery $ function to select the element by ID and set its content
+	$('#goals-needed').text(data); // If it's just text
+	// OR
+	//$('#data-container').html(data); // If you need to insert HTML
 };
 // Example data from a script
 const parsedData = '';
 
 if (parsedData == '') {
-    displayData("All goals meet.......");
+	displayData('All goals meet.......');
+} else {
+	displayData(parsedData);
 }
-else {
-    displayData(parsedData);    
-}
-

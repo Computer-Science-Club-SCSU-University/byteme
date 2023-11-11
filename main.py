954e45ed81360862b199a30d7d2df3e5822f5c03
Abdi <aabdi5520@gmail.com>
2023-11-10 20:37:25 -0600

    Basic API

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ import requests
 import uvicorn
 app = FastAPI()
 
-# origins = [
-#     "http://localhost.tiangolo.com",
-#     "https://localhost.tiangolo.com",
-#     "http://localhost",
-#     "http://localhost:8080",
-# ]
-
 
 app.add_middleware(
     CORSMiddleware,

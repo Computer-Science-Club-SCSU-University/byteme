af49118627b4195ac83c30b8c1dc9e79b29c3276
williammunnich <williammunnich@pm.me>
2023-11-10 22:00:28 -0600

    Problem was had to switch tabs

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
+#for error handling
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
 #For opening THE .env file
 import os
 from dotenv import load_dotenv
@@ -38,12 +41,23 @@ time.sleep(4)
 button_on_page = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[3]/td/select/option[33]')
 button_on_page.click()
 time.sleep(4)"""
-input_field_username = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[1]/td/input')
-input_field_password = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[2]/td/input')
-input_field_username.send_keys(username)
-input_field_password.send_keys(password)
-button_on_page = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[5]/td[2]/input')
-button_on_page.click()
+try:
+    input_field_username = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[1]/td/input')
+    input_field_password = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[2]/td/input')
+    input_field_username.send_keys(username)
+    input_field_password.send_keys(password)
+    button_on_page = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[1]/form/table/tbody/tr[5]/td[2]/input')
+    button_on_page.click()
+except NoSuchElementException as e:
+    print(f"Error: {e}")
+    # Handle the exception or pass
+    pass
+except TimeoutException as e:
+    print(f"Page load timed out: {e}")
+    pass
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+    pass
 move_on = input("Move on 1 -->")
 button_on_page = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/ul/li[5]/a')
 button_on_page.click()
@@ -54,22 +68,61 @@ move_on = input("Move on 3 -->")
 button_on_page = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div/p[3]/strong[1]/a')
 button_on_page.click()
 move_on = input("Move on 4 -->")
+# First, get the current window handle so you can switch back later if needed
+original_window = driver.current_window_handle
+
+# Perform the action that opens the new tab
+# ...
+
+# Wait for the new tab to open
+wait.until(EC.number_of_windows_to_be(2))
+
+# Store all window handles in a list
+window_handles = driver.window_handles
+
+# Switch to the new window/tab
+for handle in window_handles:
+    if handle != original_window:  # If the handle is not the original tab, switch to the new tab
+        driver.switch_to.window(handle)
+        break
 #breaking here
-input_field_username2 = driver.find_element(By.XPATH, '/html/body/main/div/div/div/form/div[1]/input')
-move_on = input("Move on 5 -->")
-input_field_password2 = driver.find_element(By.XPATH, '//*[@id="password"]')
-move_on = input("Move on 6 -->")
-input_field_username2.send_keys(username)
-move_on = input("Move on 7 -->")
-input_field_password2.send_keys(password)
-move_on = input("Move on 8 -->")
-button_on_page = driver.find_element(By.XPATH, '/html/body/main/div/div/div/form/div[3]/button')
-button_on_page.click()
+try:
+    input_field_username2 = driver.find_element(By.XPATH, '//*[@name="j_username"]') #/html/body/main/div/div/div/form/div[1]/input
+    move_on = input("Move on 5 -->")
+except NoSuchElementException as e:
+    print(f"Error: {e}")
+    # Handle the exception or pass
+    pass
+except TimeoutException as e:
+    print(f"Page load timed out: {e}")
+    pass
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+    pass
+try:
+    input_field_password2 = driver.find_element(By.XPATH, '//*[@name="j_password"]') #/html/body/main/div/div/div/form/div[2]/input
+    move_on = input("Move on 6 -->")
+    input_field_username2.send_keys(username)
+    move_on = input("Move on 7 -->")
+    input_field_password2.send_keys(password)
+    move_on = input("Move on 8 -->")
+    button_on_page = driver.find_element(By.XPATH, '/html/body/main/div/div/div/form/div[3]/button')
+    button_on_page.click()
+except NoSuchElementException as e:
+    print(f"Error: {e}")
+    # Handle the exception or pass
+    pass
+except TimeoutException as e:
+    print(f"Page load timed out: {e}")
+    pass
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+    pass
 move_on = input("Move on 9 -->")
 button_on_page = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/select')
 button_on_page.click()
 move_on = input("Move on 10 -->")
-button_on_page = driver.find_element(By.XPATH, '//*[@value="73:Instidq#002362  :Instid#   :Instcd"]')
+button_on_page = driver.find_element(By.XPATH, '//*[@value="73:Instidq#002377  :Instid#   :Instcd"]')
 button_on_page.click()
 move_on = input("Move on 11 -->")
 button_on_page = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/button')

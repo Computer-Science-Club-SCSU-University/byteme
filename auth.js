18c92b76a567daba060274a68dff54094f462391
williammunnich <williammunnich@pm.me>
2023-11-12 11:08:19 -0600

    trying to get auth working - small chages

--- auth.js
+++ auth.js
@@ -1,19 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/save', (req, res) => {
+app.post("/save", (req, res) => {
   const { username, password } = req.body;
 
   // Here, you can save the username and password to a database or file securely.
   // Do not save passwords in plaintext; always hash them before storing.
 
-  res.send('Data saved successfully');
+  // For demonstration purposes, we'll just log the received data.
+  console.log("Received Data - Username:", username);
+  console.log("Received Data - Password:", password);
+
+  res.send("Data saved successfully");
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
